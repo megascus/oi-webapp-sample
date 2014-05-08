9b7c24b86fd12044782b4675b7b259cf50d9ba53
Satoshi Kubo <megascus@gmail.com>
2014-05-09 04:45:30 +0900

    見つからなかった場合にExceptionが発生していたのを修正。

--- src/com/oisix/sample/dao/MstCustomerRepository.java
+++ src/com/oisix/sample/dao/MstCustomerRepository.java
@@ -20,24 +20,27 @@ import org.apache.commons.lang3.StringUtils;
  */
 @Stateless
 public class MstCustomerRepository extends AbstractRepository<MstCustomer> {
-
+    
     @PersistenceContext(unitName = "oi-webapp-samplePU")
     private EntityManager em;
-
+    
     @Override
     protected EntityManager getEntityManager() {
         return em;
     }
-
+    
     public MstCustomerRepository() {
         super(MstCustomer.class);
     }
-
+    
     public Optional<MstCustomer> findByCustomerId(String customerId) {
-        MstCustomer c = (MstCustomer) em.createQuery("select c from MstCustomer c where c.customerId = :customerId").setParameter("customerId", customerId).getSingleResult();
-        return Optional.ofNullable(c);
+        List<MstCustomer> c = (List<MstCustomer>) em.createQuery("select c from MstCustomer c where c.customerId = :customerId").setParameter("customerId", customerId).getResultList();
+        if (c.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(c.get(0));
     }
-
+    
     @SuppressWarnings("unchecked")
     public List<MstCustomer> findByCondition(MstCustomer searchCondition) {
         StringBuilder buff = new StringBuilder()
@@ -53,7 +56,7 @@ public class MstCustomerRepository extends AbstractRepository<MstCustomer> {
         //addSearchCodition(buff, "tel", searchCondition.getTel());
         buff.append("1 = 1 ");
         buff.append("order by e.customerId");
-
+        
         Query query = em.createQuery(buff.toString());
 
         //setParameter(query, "customerId", searchCondition.getCustomerId());
@@ -64,29 +67,29 @@ public class MstCustomerRepository extends AbstractRepository<MstCustomer> {
         //setParameter(query, "tel", searchCondition.getTel());
         return query.getResultList();
     }
-
+    
     private static void addSearchCodition(StringBuilder buff, String columnName, String parameter) {
         if (!StringUtils.isEmpty(parameter)) {
             buff.append(" ").append(columnName).append(" = :").append(columnName).append(" and ");
         }
     }
-
+    
     private static void addSearchCoditionLike(StringBuilder buff, String columnName, String parameter) {
         if (!StringUtils.isEmpty(parameter)) {
             buff.append(" ").append(columnName).append(" like :").append(columnName).append(" and ");
         }
     }
-
+    
     private static void setParameter(Query query, String columnName, String parameter) {
         if (!StringUtils.isEmpty(parameter)) {
             query.setParameter(columnName, parameter);
         }
     }
-
+    
     private static void setParameterLike(Query query, String columnName, String parameter) {
         if (!StringUtils.isEmpty(parameter)) {
             query.setParameter(columnName, "%" + parameter + "%");
         }
     }
-
+    
 }
